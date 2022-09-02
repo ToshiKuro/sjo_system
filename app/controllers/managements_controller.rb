3e9fc33f20ad5f3fb82bae855e4aaa8adc03aab9
ToshiKuro <toshi.kurochin@gmail.com>
2022-09-02 12:50:41 +0900

    test用view作成

--- app/controllers/managements_controller.rb
+++ app/controllers/managements_controller.rb
@@ -1,7 +1,12 @@
 class ManagementsController < ApplicationController
+
   def index
     select_date = DateTime.now.strftime("%d%b%y")
     FlightDatum.get_table(select_date)
     @table_data = FlightDatum.where(date: select_date)
   end
+
+  def test
+  end
+
 end
