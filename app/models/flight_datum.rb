b2322b2ea1d96ee2016bceb4e736d42361d5b040
ToshiKuro <toshi.kurochin@gmail.com>
2022-09-16 23:54:27 +0900

    option追加

--- app/models/flight_datum.rb
+++ app/models/flight_datum.rb
@@ -6,6 +6,8 @@ class FlightDatum < ApplicationRecord
       Selenium::WebDriver::Chrome.path = '/app/.apt/usr/bin/google-chrome'
       options = Selenium::WebDriver::Chrome::Options.new
       options.add_argument('--headless')
+      options.add_argument('--no-sandbox')
+      options.add_argument('--disable-dev-shm-usage')
       driver  = Selenium::WebDriver.for :chrome, capabilities: [options]
       #waitに60秒のタイマーを持たせる
       wait    = Selenium::WebDriver::Wait.new(timeout: 120)
