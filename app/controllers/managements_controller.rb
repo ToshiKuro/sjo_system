79c94f3265c942347478dd19b55c1e28296179e2
ToshiKuro <toshi.kurochin@gmail.com>
2022-09-22 12:49:12 +0900

    メール内容の変更及びデータ検索範囲の変更

--- app/controllers/managements_controller.rb
+++ app/controllers/managements_controller.rb
@@ -4,13 +4,14 @@ class ManagementsController < ApplicationController
     #検索する日にちを選択
     if params[:date].present?
       select_date = params[:date].to_date
+      jst_data    = get_jst_data(select_date)
     else
-      select_date = DateTime.now
+      select_date = Date.today
     end
 
     #当日よりも古いデータはDBから、当日以降はWebから取り込む
-    if select_date.strftime('%Y-%m-%d') < DateTime.now.strftime('%Y-%m-%d')
-      @table_data = FlightDatum.where(select_date.strftime('%d%b%y'))
+    if select_date < Date.today && jst_data.present?
+      @table_data = jst_data.map { |datum| datum.attributes.values}
     else
       @table_data = FlightDatum.get_table(select_date.strftime('%d%b%y'))
     end
@@ -20,6 +21,13 @@ class ManagementsController < ApplicationController
 
   def forward_arrival_information
     ArrivalInformation.get_arrival_information
+  end
+
+  def get_jst_data(select_date)
+    jst_data = FlightDatum.where(date: (select_date - 1).strftime('%d%b%y'),
+                                 scheduled_time_of_departure: ['15:00'..'23:59'])
+             + FlightDatum.where(date: select_date.strftime('%d%b%y'),
+                                 scheduled_time_of_departure: ['00:00'..'14:59'])
   end
 
 end
--- app/mailers/arrival_mailer.rb
+++ app/mailers/arrival_mailer.rb
@@ -1,7 +1,9 @@
 class ArrivalMailer < ApplicationMailer
 
   def forward_mail(arrival_msg)
-    @arrival_msg = arrival_msg
+    indention_point = arrival_msg.index('DT JDL')
+    @second_msg     = arrival_msg.slice!(indention_point..-1).chomp
+    @first_msg      = arrival_msg.chomp
 
     mail(from: Rails.application.credentials.gmail[:from],
          to:   Rails.application.credentials.outlook[:address])
