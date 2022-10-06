436ac6e0ce09fccbee37c1ca77f4dbda23cd866e
ToshiKuro <toshi.kurochin@gmail.com>
2022-10-06 16:47:49 +0900

    Heroku上でのScraping状況確認

--- app/models/arrival_information.rb
+++ app/models/arrival_information.rb
@@ -9,7 +9,7 @@ class ArrivalInformation
 
       Selenium::WebDriver::Chrome.path = '/app/.apt/usr/bin/google-chrome'
       options = Selenium::WebDriver::Chrome::Options.new
-      options.add_argument('--headless')
+      #options.add_argument('--headless')
       options.add_argument('--no-sandbox')
       options.add_argument('--disable-dev-shm-usage')
       driver  = Selenium::WebDriver.for :chrome, capabilities: [options]
--- app/models/flight_datum.rb
+++ app/models/flight_datum.rb
@@ -6,7 +6,7 @@ class FlightDatum < ApplicationRecord
     def get_table(select_date)
       Selenium::WebDriver::Chrome.path = '/app/.apt/usr/bin/google-chrome'
       options = Selenium::WebDriver::Chrome::Options.new
-      options.add_argument('--headless')
+      #options.add_argument('--headless')
       options.add_argument('--no-sandbox')
       options.add_argument('--disable-dev-shm-usage')
       driver  = Selenium::WebDriver.for :chrome, capabilities: [options]
