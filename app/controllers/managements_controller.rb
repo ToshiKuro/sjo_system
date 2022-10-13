bd744ca332df92fa8963781cea2e213007e27eee
ToshiKuro <toshi.kurochin@gmail.com>
2022-10-13 10:30:23 +0900

    Excelデータの日付による分割

--- app/controllers/managements_controller.rb
+++ app/controllers/managements_controller.rb
@@ -9,11 +9,13 @@ class ManagementsController < ApplicationController
       select_date = Date.today
     end
 
+    excel_data = get_excel_data(select_date)
+
     #当日よりも古いデータはDBから、当日以降はExcelから取り込む
     if select_date < Date.today && jst_data.present?
       @table_data = jst_data.map { |datum| datum.attributes.values}
-    elsif select_date >= Date.today && get_excel_data.present?
-      @table_data = get_excel_data
+    elsif select_date >= Date.today && excel_data.present?
+      @table_data = excel_data
     else
       @table_data = []
     end
@@ -21,7 +23,7 @@ class ManagementsController < ApplicationController
     @select_date = select_date.strftime('%Y-%m-%d')
   end
 
-  def get_excel_data
+  def get_excel_data(select_date)
     xlsx        = Roo::Excelx.new("#{Dir.home}/Desktop/SEND_ARR_INFO.xlsx")
     max_row_num = xlsx.last_row
     excel_data  = []
@@ -30,7 +32,13 @@ class ManagementsController < ApplicationController
       excel_data << xlsx.sheet('schedule').row(i + 1)
     end
 
-    excel_data
+    next_day_index = excel_data.find_index { |row| row[1] == Date.today.strftime('%d%b%y') && row[7] >= "15:00"}
+
+    if select_date == Date.today
+      excel_data[0..(next_day_index - 1)]
+    else
+      excel_data[next_day_index..-1]
+    end
   end
 
   def forward_arrival_information
