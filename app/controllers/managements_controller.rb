29219fc96214bd47454dc3aa4329fae4e0699c8f
ToshiKuro <toshi.kurochin@gmail.com>
2022-10-13 18:11:01 +0900

    flight data取得のmethod化と不要methodの削除

--- app/controllers/managements_controller.rb
+++ app/controllers/managements_controller.rb
@@ -8,20 +8,23 @@ class ManagementsController < ApplicationController
       select_date = Date.today
     end
 
-    #N-OC APIからデータを取り込む
-    client   = Savon.client(:wsdl => Rails.application.credentials.soap_ui[:wsdl])
-    response = client.call(:get_flights,
-                           :message => {'Username'            => Rails.application.credentials.noc[:id],
-                                        'Password'            => Rails.application.credentials.noc[:pw],
-                                        'FlightRequestFilter' => {'From'         => '2022-10-12T00:00:00',
-                                                                  'To'           => '2022-10-12T23:00:00'},
-                                        'FlightRequestData'   => {'Airports'     => 'true',
-                                                                  'Times'        => 'true',
-                                                                  'Loads'        => 'true',
-                                                                  'CrewOnBoards' => 'true'} })
-
+    @select_date = select_date.strftime('%Y-%m-%d')
     @table_data  = []
+    get_table_data(select_date)
+  end
 
+  def get_table_data(select_date)
+    #N-OC APIからデータを取り込む
+    client    = Savon.client(:wsdl => Rails.application.credentials.soap_ui[:wsdl])
+    response  = client.call(:get_flights,
+                            :message => {'Username'            => Rails.application.credentials.noc[:id],
+                                         'Password'            => Rails.application.credentials.noc[:pw],
+                                         'FlightRequestFilter' => {'From'         => select_date.strftime('%Y-%m-%dT00:00:00'),
+                                                                   'To'           => select_date.strftime('%Y-%m-%dT23:59:59')},
+                                         'FlightRequestData'   => {'Airports'     => 'true',
+                                                                   'Times'        => 'true',
+                                                                   'Loads'        => 'true',
+                                                                   'CrewOnBoards' => 'true'} })
     response.body[:get_flights_response][:get_flights_result][:flight].each do |flight|
       next if flight[:times] == nil
       flight_times     = flight[:times][:time]
@@ -49,43 +52,12 @@ class ManagementsController < ApplicationController
                         flight_loads[:adults],
                         flight_loads[:children],
                         flight_loads[:infants],
-                        block_off[:date_time].strftime('%H%M'),
-                        take_off[:date_time].strftime('%H%M'),
+                        block_off      == nil ? '' : block_off[:date_time].strftime('%H%M'),
+                        take_off       == nil ? '' : take_off[:date_time].strftime('%H%M'),
                         estimated_time == nil ? '' : estimated_time[:date_time].strftime('%H%M'),
-                        landing[:date_time].strftime('%H%M'),
-                        block_on[:date_time].strftime('%H%M') ]
+                        landing        == nil ? '' : landing[:date_time].strftime('%H%M'),
+                        block_on       == nil ? '' : block_on[:date_time].strftime('%H%M') ]
     end
-
-    @select_date = select_date.strftime('%Y-%m-%d')
-  end
-
-  def get_excel_data(select_date)
-    xlsx        = Roo::Excelx.new("#{Dir.home}/Desktop/SEND_ARR_INFO.xlsx")
-    max_row_num = xlsx.last_row
-    excel_data  = []
-
-    max_row_num.times do |i|
-      excel_data << xlsx.sheet('schedule').row(i + 1)
-    end
-
-    next_day_index = excel_data.find_index { |row| row[1] == Date.today.strftime('%d%b%y') && row[7] >= "15:00"}
-
-    if select_date == Date.today
-      excel_data[0..(next_day_index - 1)]
-    else
-      excel_data[next_day_index..-1]
-    end
-  end
-
-  def forward_arrival_information
-    ArrivalInformation.get_arrival_information
-  end
-
-  def get_jst_data(select_date)
-    jst_data = FlightDatum.where(date: (select_date - 1).strftime('%d%b%y'),
-                                 scheduled_time_of_departure: ['15:00'..'23:59']) +
-               FlightDatum.where(date: select_date.strftime('%d%b%y'),
-                                 scheduled_time_of_departure: ['00:00'..'14:59'])
   end
 
 end
--- app/javascript/packs/management.js
+++ app/javascript/packs/management.js
@@ -1,21 +1,13 @@
 $(function(){
 
   if ($('#title').text() == '運航管理画面') {
-    id1 = setInterval(function() {
+    id = setInterval(function() {
       location.reload();
     }, 300000);
   }
 
-  id2 = setInterval(function() {
-    $.ajax({
-      url:  '/forward_arrival_information',
-      type: 'get'
-    })
-  }, 180000);
-
   $('#title').click(function() {
-    clearInterval(id1);
-    clearInterval(id2);
+    clearInterval(id);
   })
 
 });
