80274fdfbf09c32df53662a4412990df93bb930c
ToshiKuro <toshi.kurochin@gmail.com>
2022-10-13 17:10:35 +0900

    flight dataの追加

--- app/controllers/managements_controller.rb
+++ app/controllers/managements_controller.rb
@@ -13,11 +13,49 @@ class ManagementsController < ApplicationController
     response = client.call(:get_flights,
                            :message => {'Username'            => Rails.application.credentials.noc[:id],
                                         'Password'            => Rails.application.credentials.noc[:pw],
-                                        'FlightRequestFilter' => {'From'     => '2022-10-12T00:00:00',
-                                                                  'To'       => '2022-10-12T23:00:00'},
-                                        'FlightRequestData'   => {'Aircraft' => 'true'}})
+                                        'FlightRequestFilter' => {'From'         => '2022-10-12T00:00:00',
+                                                                  'To'           => '2022-10-12T23:00:00'},
+                                        'FlightRequestData'   => {'Airports'     => 'true',
+                                                                  'Times'        => 'true',
+                                                                  'Loads'        => 'true',
+                                                                  'CrewOnBoards' => 'true'} })
 
-    @table_data  = response.body[:get_flights_response][:get_flights_result][:flight]
+    @table_data  = []
+
+    response.body[:get_flights_response][:get_flights_result][:flight].each do |flight|
+      next if flight[:times] == nil
+      flight_times     = flight[:times][:time]
+      flight_loads     = flight[:loads][:booked_passenger_per_weight]
+      pilot_in_command = flight[:crew_on_board][:crew_on_board].find { |crew| crew[:assigned_rank] == 'CP' }
+      block_off        = flight_times.find { |time| time[:type] == 'ActualBlockOff' }
+      take_off         = flight_times.find { |time| time[:type] == 'ActualTakeOff' }
+      estimated_time   = flight_times.find { |time| time[:type] == 'EstimatedBlockOn' }
+      landing          = flight_times.find { |time| time[:type] == 'ActualTouchDown' }
+      block_on         = flight_times.find { |time| time[:type] == 'ActualBlockOn' }
+
+      @table_data  << [ flight[:unique_id],
+                        flight[:flight_date].strftime('%d%b%y'),
+                        'SJO' + flight[:flight_number],
+                        flight[:aircraft_registration],
+                        pilot_in_command[:crew][:nickname],
+                        #crew_configuration
+                        flight[:departure_airport_code],
+                        flight[:std].strftime('%H:%M'),
+                        flight[:airports][:departure_airport][:stand],
+                        flight[:arrival_airport_code],
+                        flight[:sta].strftime('%H:%M'),
+                        flight[:airports][:arrival_airport][:stand],
+                        Time.at(flight[:sta].to_time - flight[:std].to_time).utc.strftime('%H:%M'),
+                        flight_loads[:adults],
+                        flight_loads[:children],
+                        flight_loads[:infants],
+                        block_off[:date_time].strftime('%H%M'),
+                        take_off[:date_time].strftime('%H%M'),
+                        estimated_time == nil ? '' : estimated_time[:date_time].strftime('%H%M'),
+                        landing[:date_time].strftime('%H%M'),
+                        block_on[:date_time].strftime('%H%M') ]
+    end
+
     @select_date = select_date.strftime('%Y-%m-%d')
   end
 
